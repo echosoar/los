e304f6df7c3ecdc9872a18788ab1951f1a0dabaa
echosoar <82163514@qq.com>
2017-12-07 00:59:09 -0500

    update add default index file

--- server.js
+++ server.js
@@ -1,13 +1,17 @@
 'use strict';
 
 let parseurl = require('parseurl');
+let path = require('path');
 
 class Server {
-	constructor(req, res) {
+	constructor(req, res, options) {
 		this.req = req;
 		this.res = res;
-		this.info = {};
+	
+		options = options || {};
 
+		this.defaultIndex = options.defaultIndex || ['index.html', 'index.htm'];
+		this.rootDirAddress = option.root || process.cwd();
 		this._parseUrl();
 	}
 	
@@ -53,6 +57,37 @@ class Server {
 		this.location.path = path;
 		this.location.queryParams = this._toParams(url.query);
 
+	}
+
+
+	_fileExists(filePath, fileName) {
+		if (fileName) {
+			filePath = path.resolve(filePath, './' + fileName);
+		}
+		filePath = path.resolve(this.rootDirAddress, './' + filePath); 
+	}
+
+	_targetFile() {
+		let filePath = this.location.path.replace(/^\//, ''); // transform /a/b.html/ to a/b.htm/
+		if (/\/$/.test(filePath) { // if filePath is a/b.html/ , transform to a/b.html
+			filePath = filePath.replace(/\/$/, '');	
+		}
+		let filePathArr = filePath.split('/');
+		let indexFile = false;
+		// not exists file path , redirect to default  index
+		if (!filePathArr.length || !/\.[a-z]+/i.test(filePathArr[filePathArr.length -1]) {
+			for (let index = 0; index < this.defaultIndex.length; index ++) {
+				if (this._fileExists(filePath, this.defaultIndex[index]) {
+					indexFile = this.defaultIndex[index];
+					break;
+				}
+			}	
+		} else {
+			if (this._fileExists(filePath)) {
+				indexFile = filePathArr.pop();
+			}
+		}
+		
 	}
 }
 
