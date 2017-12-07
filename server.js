875237ef6f8500e5d4e7331fab5ab67ac60dcee9
echosoar <82163514@qq.com>
2017-12-07 06:22:05 -0500

    com file

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 
 let parseurl = require('parseurl');
 let path = require('path');
+let fs = require('fs');
 
 class Server {
 	constructor(req, res, options) {
@@ -10,9 +11,22 @@ class Server {
 	
 		options = options || {};
 
+		this._getExtMap();
+
 		this.defaultIndex = options.defaultIndex || ['index.html', 'index.htm'];
-		this.rootDirAddress = option.root || process.cwd();
+		this.rootDirAddress = options.root || process.cwd();
 		this._parseUrl();
+		this._targetFile();
+	}
+
+
+	_getExtMap() {
+		this.extMap = {
+			json: 'application/json',
+			js: 'text/javascript',
+			css: 'text/css',
+		  	html: 'text/html'
+		};
 	}
 	
 	/*
@@ -62,22 +76,44 @@ class Server {
 
 	_fileExists(filePath, fileName) {
 		if (fileName) {
-			filePath = path.resolve(filePath, './' + fileName);
+			filePath = path.resolve(this.rootDirAddress, './' + filePath, './' + fileName);
+		} else {
+			filePath= path.resolve(this.rootDirAddress, './' + filePath); 
 		}
-		filePath = path.resolve(this.rootDirAddress, './' + filePath); 
+		this.fileAbsolutePath = filePath;
+		return fs.existsSync(filePath);
+	}
+
+	_handle404(filePath) {
+		this.res.writeHead(404);
+		this.res.end();
+	}
+
+	_handleListDir(filePath) {}
+	_handleReadFile() {
+		let ext = /\.([^\.]+)$/.exec(this.fileAbsolutePath);
+  		ext = ext[1].toLowerCase();
+
+		if (this['_execFileType_' + ext]) {
+			this['_execFileType_' + ext]();
+		} else {
+			let ctp = this.extMap[ext] || 'text/plain';
+  			this.res.writeHead(200, {'Content-Type': ctp});
+  			this.res.end(fs.readFileSync(this.fileAbsolutePath));
+		}
 	}
 
 	_targetFile() {
 		let filePath = this.location.path.replace(/^\//, ''); // transform /a/b.html/ to a/b.htm/
-		if (/\/$/.test(filePath) { // if filePath is a/b.html/ , transform to a/b.html
+		if (/\/$/.test(filePath)) { // if filePath is a/b.html/ , transform to a/b.html
 			filePath = filePath.replace(/\/$/, '');	
 		}
 		let filePathArr = filePath.split('/');
 		let indexFile = false;
 		// not exists file path , redirect to default  index
-		if (!filePathArr.length || !/\.[a-z]+/i.test(filePathArr[filePathArr.length -1]) {
+		if (!filePathArr.length || !/\.[a-z]+/i.test(filePathArr[filePathArr.length -1])) {
 			for (let index = 0; index < this.defaultIndex.length; index ++) {
-				if (this._fileExists(filePath, this.defaultIndex[index]) {
+				if (this._fileExists(filePath, this.defaultIndex[index])) {
 					indexFile = this.defaultIndex[index];
 					break;
 				}
@@ -88,6 +124,16 @@ class Server {
 			}
 		}
 		
+		if (!indexFile) {
+			// file is not exists
+			if (this.notExists == 'readDir') {
+				this._handleListDir(filePath);
+			} else {
+				this._handle404(filePath);
+			}
+		} else {
+			this._handleReadFile();
+		}	
 	}
 }
 
